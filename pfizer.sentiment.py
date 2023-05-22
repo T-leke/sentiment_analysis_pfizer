f26e603d0ef74210d66f6e65f50e98dcc181f09f
Tolu <gbemilekeogidan@gmail.com>
2023-05-22 23:24:25 +0100

    creating a data frame of the pfizer tweets

--- pfizer.sentiment.py
+++ pfizer.sentiment.py
@@ -1,54 +1,58 @@
 import tweepy
 import psycopg2
 import os
+import datetime
 
 key = os.environ.get('consumer_key')
 secret = os.environ.get('consumer_secret')
 token = os.environ.get('access_token')
 token_secret = os.environ.get('access_token_secret')
 
-print(secret)
-# auth = tweepy.OAuthHandler(key, secret)
-# auth.set_access_token(token, token_secret)
 
-# api = tweepy.API(auth)
+auth = tweepy.OAuthHandler(key, secret)
+auth.set_access_token(token, token_secret)
 
-
-# search_query = 'Pfizer vaccine'
-# num_tweets = 1000
+api = tweepy.API(auth)
 
 
-# tweets = api.search_tweets(q=search_query, tweet_mode='extended', count=num_tweets)
-
-# for tweet in tweets:
-#     # Process or store each tweet as desired
-#     tweet_text = tweet.full_text
-#     tweet_time = tweet.created_at
+search_query = 'Pfizer vaccine'
+num_tweets = 1000
 
 
+tweets = api.search_tweets(q=search_query, tweet_mode='extended', count=num_tweets)
 
-# #pushing the tweets to the database
-# conn = psycopg2.connect(
-#     host="localhost",
-#     port="5432",
-#     database="Bookscrape",
-#     user="postgres",
-#     password="#Tolexy5038"
-# )
+conn = psycopg2.connect(
+host="localhost",
+port="5432",
+database="Bookscrape",
+user="postgres",
+password="#Tolexy5038"
+)
 
-# cur = conn.cursor()
+cur = conn.cursor()
 
-# reate_table_query = """
-# CREATE TABLE pfizer (
-# timestamp TIME,
-# Tweets VARCHAR
-# )
-# """
+for tweet in tweets:
+    # Process or store each tweet as desired
+    tweet_text = tweet.full_text
+    tweet_time = tweet.created_at
 
-# cur.execute(reate_table_query)
+    tweet_time = datetime.datetime.strftime(tweet_time, '%Y-%m-%d %H:%M:%S+00:00')
 
-# insert = "INSERT INTO pfizer (timestamp, Tweets) VALUES ({}, {})".format(tweet_time, tweet_text)
+    #pushing the tweets to the database
 
-# cur.execute(insert)
-# conn.commit()
-# cur.close()
+    tweet_text = tweet_text.replace("'", "''")
+    # create_table_query = """
+    # CREATE TABLE pfizer (
+    # timestamp TIMESTAMP,
+    # Tweets TEXT
+    # )
+    # """
+
+    # cur.execute(create_table_query)
+
+    insert = "INSERT INTO pfizer (timestamp, Tweets) VALUES ('{}', E'{}')".format(tweet_time, tweet_text)
+
+    cur.execute(insert)
+    
+conn.commit()
+cur.close()
