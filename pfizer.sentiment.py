95aaacebc37d0b7caa8d8305bb96e5cc9d4b90f9
Tolu <gbemilekeogidan@gmail.com>
2023-05-22 21:09:09 +0100

    hiding the keys

--- pfizer.sentiment.py
+++ pfizer.sentiment.py
@@ -1,27 +1,54 @@
 import tweepy
 import psycopg2
+import os
 
-consumer_key = 'oYOe9RFvv1s6Z8TyfpzXM4R5a'
-consumer_secret = '78SDg96GwxZu0jOQo6vmebQi2t7Xqu75J3HsVtxIxtarIKgS2L'
-access_token = '165197655-KwonLmnWxgQPiXR6Cq9ZMAscBMCTcny2E9cCkrmN'
-access_token_secret = 'D4UbrKFoQkr6CJwqGLWr2Dt1eKigfiHGhIPSoS8OBx0fz'
+key = os.environ.get('consumer_key')
+secret = os.environ.get('consumer_secret')
+token = os.environ.get('access_token')
+token_secret = os.environ.get('access_token_secret')
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+print(secret)
+# auth = tweepy.OAuthHandler(key, secret)
+# auth.set_access_token(token, token_secret)
 
-api = tweepy.API(auth)
-
-
-search_query = 'Pfizer vaccine'
-num_tweets = 1000
+# api = tweepy.API(auth)
 
 
-tweets = api.search_tweets(q=search_query, tweet_mode='extended', count=num_tweets)
-
-for tweet in tweets:
-    # Process or store each tweet as desired
-    tweet_text = tweet.full_text
-    tweet_time = tweet.created_at
+# search_query = 'Pfizer vaccine'
+# num_tweets = 1000
 
 
-print(tweet_time)
+# tweets = api.search_tweets(q=search_query, tweet_mode='extended', count=num_tweets)
+
+# for tweet in tweets:
+#     # Process or store each tweet as desired
+#     tweet_text = tweet.full_text
+#     tweet_time = tweet.created_at
+
+
+
+# #pushing the tweets to the database
+# conn = psycopg2.connect(
+#     host="localhost",
+#     port="5432",
+#     database="Bookscrape",
+#     user="postgres",
+#     password="#Tolexy5038"
+# )
+
+# cur = conn.cursor()
+
+# reate_table_query = """
+# CREATE TABLE pfizer (
+# timestamp TIME,
+# Tweets VARCHAR
+# )
+# """
+
+# cur.execute(reate_table_query)
+
+# insert = "INSERT INTO pfizer (timestamp, Tweets) VALUES ({}, {})".format(tweet_time, tweet_text)
+
+# cur.execute(insert)
+# conn.commit()
+# cur.close()
